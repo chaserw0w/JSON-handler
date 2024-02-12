ce680fc013caeb59fbe425a95ada10471542bf0b
Taras Kurenda <chaserw0w.ti@gmail.com>
2024-02-12 15:54:06 +0200

    initial commit with basic functionality of connecting to URL and reading response

---
+++ src/main/java/taras/com/App.java
@@ -0,0 +1,55 @@
+package taras.com;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static taras.com.utils.Properties.*;
+
+public class App {
+
+    private static final Logger LOGGER = Logger.getLogger(App.class.getName());
+
+    public static void main( String[] args ) {
+        try {
+            String initialResponse = getResponseFromUrl(ENDPOINT);
+            LOGGER.info("Initial response received: " + initialResponse);
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, "An error occurred when getting response from URL", e);
+            e.printStackTrace();
+        }
+    }
+
+    private static String getResponseFromUrl(String urlString) throws Exception {
+
+        StringBuilder response = new StringBuilder();
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+            LOGGER.info("Response code: " + responseCode);
+
+            if (responseCode > 299) {
+                LOGGER.log(Level.SEVERE, "Failed to get response from URL. Response code: " + responseCode);
+            } else {
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))){
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+                }
+            }
+            connection.disconnect();
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, "An error occurred when getting response from from URL:", e);
+            e.printStackTrace();
+        }
+
+        return response.toString();
+    }
+}
---
+++ src/main/java/taras/com/utils/Properties.java
@@ -0,0 +1,9 @@
+package taras.com.utils;
+
+/**
+    This class provides some utility names for variables
+ */
+public class Properties {
+
+    public static final String ENDPOINT = "https://jsonplaceholder.typicode.com/posts";
+}
